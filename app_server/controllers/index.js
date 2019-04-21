b444a0de601c5266de757345aa2a9dcbe9063d8c
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2019-04-21 14:16:49 +0800

    Modified Index Page
    Added jumbotron, some basic albums and style and changed routes

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -3,5 +3,5 @@ var createError = require('http-errors');
 
 
 module.exports.display = function(req, res, next) {
-    res.render('index',{title: 'start page'});
+    res.render('index',{title: 'MusicLibrary'});
   }
